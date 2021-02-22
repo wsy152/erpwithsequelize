2dbf786e055b4c9a2fc74cefd5ccb5a3ac090c29
Edvaldo Santana <edvaldosantana@Mac-mini-de-Edvaldo.local>
2021-02-22 01:55:41 -0300

    006 - Criar Rota Delete de Usuario

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -52,5 +52,30 @@ class UsuariosController {
       return res.json(null);
     }
   }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+      const usuario = await Usuarios.findByPk(id);
+      if (!usuario) {
+        return res.status(400).json({
+          errors: ['usuarios não existe'],
+        });
+      }
+      await usuario.destroy();
+      return res.json({
+        mensagem: {
+          msg: 'Usuario excluido com Sucesso',
+        },
+      });
+    } catch (error) {
+      return res.json(null);
+    }
+  }
 }
 export default new UsuariosController();
--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -7,5 +7,6 @@ router.post('/', usuariosController.store);
 router.get('/', usuariosController.index);
 router.get('/:id', usuariosController.show);
 router.put('/:id', usuariosController.update);
+router.delete('/:id', usuariosController.delete);
 
 export default router;
